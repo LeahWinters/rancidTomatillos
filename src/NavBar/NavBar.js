6a35b7508de9aab367fd443200f9dd72caeb3b8f
Leah Winters <leah.winters24@gmail.com>
2020-07-07 14:35:36 -0600

    Small clean up

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./NavBar.css";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   constructor(props) {
