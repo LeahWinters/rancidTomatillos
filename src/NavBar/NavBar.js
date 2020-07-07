34a02185696868d53dc08a40362175a00a0ca491
Leah Winters <leah.winters24@gmail.com>
2020-07-06 20:10:42 -0600

    When user clicks logout, they are brought back to the home page

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,48 +1,51 @@
-import React from 'react';
-import './NavBar.css';
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import "./NavBar.css";
+import { Link, Redirect } from "react-router-dom";
 
-const NavBar = ({ isLoggedIn }) => {
-
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  // }
-
-  if(!isLoggedIn) {
-    return (
-      <section className="NavBar">
-        <h1>Rancid Tomatillos - The Site</h1>
-        <Link to={`login`}>
-          <button 
-            className="login-btn-nav-bar" 
-            // onClick={this.handleSubmit()}
-            type='button'
-            >
-              Login
-          </button>
-        </Link>
-        
-      </section>
-    )
-  } else {
-    return (
-      <section className="NavBar">
-        <h1>Rancid Tomatillos - The Site</h1>
-        <Link to='/'>
-          <button 
-            className="login-btn-nav-bar" 
-            // onClick={this.handleSubmit()}
-            type='button'
-            >
-              Logout
-          </button>
-        </Link>
-        
-      </section>
-    )
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  
+  handleClick = (e) => {
+    e.preventDefault();
+    this.props.signOut();
+  };
+
+  render() {
+
+    if (!this.props.isLoggedIn) {
+      return (
+        <section className="NavBar">
+          <h1>Rancid Tomatillos - The Site</h1>
+          <Link to={`login`}>
+            <button
+              className="login-btn-nav-bar"
+              type="button"
+            >
+              Login
+            </button>
+          </Link>
+        </section>
+      );
+    } else {
+      return (
+        <section className="NavBar">
+          <h1>Rancid Tomatillos - The Site</h1>
+          <Link to="/">
+            <button
+              className="login-btn-nav-bar"
+              onClick={this.handleClick}
+              type="button"
+            >
+              Logout
+            </button>
+          </Link>
+        </section>
+      );
+    }
+  }
 }
 
 export default NavBar;
